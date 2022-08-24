119c0c51844752adedc70f1d04793b17fb60bef9
Danillo Silva <danillordm@gmail.com>
2022-08-23 22:28:43 -0300

    Add Aviao class

--- src/lista_1/Aviao.java
+++ src/lista_1/Aviao.java
@@ -6,19 +6,18 @@ que fazem parte da rota da aeronave.
 *******************************************************************************/
 
 package lista_1;
-//Incompleto!
 
 import java.util.Scanner;
 
 public class Aviao {
-	public static void percorrer() {
+	public static void viagem() {
 		Scanner scan = new Scanner(System.in);
 		String cidades[] = { "Recife", "João Pessoa", "Maceió", "Natal", "Aracajú", "Fortaleza", "Salvador" };
 		int distancias[][] = new int[7][7];
 		int cidade1, cidade2, total;
 
-		for (int i = 0; i < cidades.length; i++) {
-			for (int j = 0; j < cidades.length; j++) {
+		for (int i = 0; i < cidades.length; i++) { // linha
+			for (int j = 0; j < cidades.length; j++) { // coluna
 				if (i == j) {
 					distancias[i][j] = 0;
 				} else if ((i == 0 && j == 1) || (i == 1 && j == 0) || (i == 1 && j == 5) || (i == 3 && j == 5)
@@ -47,43 +46,54 @@ public class Aviao {
 					distancias[i][j] = 13;
 				} else if ((i == 5 && j == 6) || (i == 6 && j == 5)) {
 					distancias[i][j] = 14;
+				} else if ((i == 2 && j == 5) || (i == 5 && j == 2)) {
+					distancias[i][j] = 3;
+				} else {
+					distancias[i][j] = 4;
 				}
 			}
 		}
 
 //		for (int i = 0; i < cidades.length; i++) {
 //			for (int j = 0; j < cidades.length; j++) {
-//				System.out.print(" [" + distancias[i] [j] + "] ");
+//				System.out.print(" [" + distancias[i][j] + "] ");
 //			}
 //			System.out.println();
 //		}
-	
+
 		for (int i = 0; i < cidades.length; i++) {
-			System.out.printf("%d. %s \n",(i + 1), cidades[i]);
+			System.out.printf("%d. %s \n", (i + 1), cidades[i]);
 		}
 
 		do {
-			cidade1 = -1;
-			cidade2 = -1;
 			total = 0;
+			System.out.println("\nEscolha os codigos das cidades ou valores repetidos para interromper. \n");
 			System.out.println("Digite o número da 1ª cidade: ");
-			cidade1 = scan.nextInt();
+			cidade1 = scan.nextInt() - 1;
+			System.out.printf("Você escolheu a cidade %s \n\n", cidades[cidade1]);
 
 			System.out.println("Digite o número da 2ª cidade: ");
-			cidade2 = scan.nextInt();
+			cidade2 = scan.nextInt() - 1;
+			System.out.printf("Você escolheu a cidade %s \n\n", cidades[cidade2]);
+			
+			if(cidade1 == cidade2) {
+				break;
+			}
 
-//			for (int i = 0; i < cidade2; i++) {
-//				for (int j = cidade1; j < cidade2; j++) {
-//					total += distancias[i][j];
-//				}
-//			}
+			for (int i = cidade1; i <= cidade1; i++) {
+				for (int j = 0; j <= cidade2; j++) {
+					total += distancias[i][j];
+					System.out.println(distancias[i][j]);
+				}
+			}
 
-			System.out.println(total);
+			System.out.printf("De %s até a %s são %d horas \n\n", cidades[cidade1], cidades[cidade2], total);
+			
 		} while (cidade1 != cidade2);
 
 	}
 
 	public static void main(String[] args) {
-		percorrer();
+		viagem();
 	}
 }
