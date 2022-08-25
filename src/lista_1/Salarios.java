8a01225a5b91536d43b83629711c351d6e7f8c38
Danillo Silva <33943534+Lillow@users.noreply.github.com>
2022-08-25 12:41:23 -0300

    Update Salarios.java

--- src/lista_1/Salarios.java
+++ src/lista_1/Salarios.java
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-18. Escreva uma classe que receberá o nome e o salário de cinco funcionários e aplique o reajuste salarial
+17. Escreva uma classe que receberá o nome e o salário de cinco funcionários e aplique o reajuste salarial
 de acordo com a tabela abaixo. Ao final informe o salário anterior, o valor que será acrescido após
 aplicar o percentual de reajuste e também informe o novo valor de salário de cada funcionário.
 Também informe qual o valor total que a empresa terá a mais em sua folha de pagamento após
