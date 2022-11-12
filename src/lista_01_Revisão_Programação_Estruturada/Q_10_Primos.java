8095ba09020f69cea2f32319387a3f9195138d66
Danillo Silva <danillordm@gmail.com>
2022-11-12 15:22:29 -0300

    Rename all questions from list 1

---
+++ src/lista_01_Revisão_Programação_Estruturada/Q_10_Primos.java
@@ -0,0 +1,28 @@
+package lista_01_Revisão_Programação_Estruturada;
+
+import java.util.Scanner;
+
+//10. Escreva uma classe que receba um número qualquer do usuário e identifique se é um número primo.
+
+public class Q_10_Primos {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num;
+		boolean primo = true;
+
+		System.out.println("Entre com um numero ");
+		num = scan.nextInt();
+
+		for (int i = 2; i < num; i++) {
+			if(num % i == 0) {
+				primo = false;
+			}
+			
+		}
+		if (primo) {
+			System.out.println("É primo!");
+		}else {
+			System.out.println("Não é pimo!");
+		}
+	}
+}
---
+++ src/lista_01_Revisão_Programação_Estruturada/Q_11_Fatorial.java
@@ -0,0 +1,25 @@
+package lista_01_Revisão_Programação_Estruturada;
+
+import java.util.Scanner;
+
+//11. Escreva um algoritmo que receba um número qualquer do usuário e calcule o fatorial deste número.
+
+public class Q_11_Fatorial {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num;
+		
+		System.out.println("Digite um número para ver o seu fatorial: ");
+		num = scan.nextInt();
+		
+		System.out.print(num + "! = " + num + " x ");
+		for (int i = num -1; i > 0; i--) {
+			num = num * i;
+			System.out.print(i);
+			if (i != 1) {
+				System.out.print(" x ");
+			}
+		}
+		System.out.println("\nResultado: " + num);
+	}
+}
