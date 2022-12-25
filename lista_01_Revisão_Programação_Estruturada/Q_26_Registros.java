88e6b189f505434b02118e79e164d15f65188963
Danillo Silva <danillordm@gmail.com>
2022-12-25 17:14:49 -0300

    Fixs

---
+++ lista_01_Revisão_Programação_Estruturada/Q_26_Registros.java
@@ -0,0 +1,34 @@
+package lista_01_Revisão_Programação_Estruturada;
+
+import java.util.Scanner;
+
+/********************************
+ * 26. Crie um programa que armazene os dados de registros do aluno abaixo
+ * contendo 20 registros. Depois elabore um algoritmo capaz de responder às
+ * questões, sendo que os registros estão organizados por série, turma e nome.
+ * 
+ * a. Qual a porcentagem de alunos aprovados e reprovados por turma, totalizados
+ * por série?
+ * 
+ * b. Qual a porcentagem de alunos do sexo masculino e do sexo feminino por
+ * turma, totalizados por série?
+ * 
+ * c. Qual a média das idades de cada uma das séries?
+ * 
+ * d. Qual a porcentagem de alunos (estrangeiros) de outras cidades na escola?
+ * 
+ * e. Qual a porcentagem de alunos repetentes em cada turma?
+ * 
+ * f. Quais os cinco melhores alunos de cada série (em ordem crescente)?
+ ********************************/
+
+public class Q_26_Registros {
+	public static void cadastro() {
+		String matricula, nome, naturalidades;
+		int serie, anoNascimento;
+		char turma, sexo;
+		float media;
+		boolean aprovado;
+		Scanner escreva = new Scanner(System.in);
+	}
+}
